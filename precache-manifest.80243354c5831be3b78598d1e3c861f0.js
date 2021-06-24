b96245e9aa00325749b2de077b1283f229d3840c
do.an <doan.cdv@gmail.com>
2021-06-24 16:35:12 +0700

    first commit

---
+++ precache-manifest.80243354c5831be3b78598d1e3c861f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77af813fa2b65847d6492dd71edda75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b69edc89b9dd40f8189d",
+    "url": "/static/css/2.920b99fa.chunk.css"
+  },
+  {
+    "revision": "3e540f6eeaebc8a8c493",
+    "url": "/static/css/main.1533d125.chunk.css"
+  },
+  {
+    "revision": "b69edc89b9dd40f8189d",
+    "url": "/static/js/2.572c7bc9.chunk.js"
+  },
+  {
+    "revision": "38586356ccd265e93610d2a704cc6735",
+    "url": "/static/js/2.572c7bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e540f6eeaebc8a8c493",
+    "url": "/static/js/main.77c606cc.chunk.js"
+  },
+  {
+    "revision": "4bfb9c89679681598b44",
+    "url": "/static/js/runtime-main.9867c99f.js"
+  }
+]);
